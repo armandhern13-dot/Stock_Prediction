889be9c6e284871086d55794dd6c807df9b7b03e
armandhern13-dot <armandhern13@gmail.com>
2026-03-26 20:59:34 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -53,15 +53,14 @@ def extract_features_pair():
 
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['AAPL', 'MPWR']
+    stk_tickers = ['NVDA', 'AME']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
 
-    Y = stk_data.loc[:, ('Adj Close', 'AAPL')]
-    Y.name = 'AAPL'
-
-    X = stk_data.loc[:, ('Adj Close', 'MPWR')]
-    X.name = 'MPWR'
+   Y = stk_data.loc[:, ('Adj Close', 'NVDA')]
+Y.name = 'NVDA'
+X = stk_data.loc[:, ('Adj Close', 'AME')]
+X.name = 'AME'
 
     dataset = pd.concat([Y, X], axis=1).dropna()
     Y = dataset.loc[:, Y.name]
